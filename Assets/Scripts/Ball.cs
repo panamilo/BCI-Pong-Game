8cf5966dddce30db0781e18d38dcb2373622fc8a
giorgos15 <matageorge266@gmail.com>
2022-11-26 22:21:17 +0200

    sxedon etoimo ena vhma leipei

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -6,13 +6,24 @@ public class Ball : MonoBehaviour
 
     private Rigidbody2D _rigidbody;
 
-    private void Awake() {
+    private void Awake() 
+    {
         _rigidbody = GetComponent<Rigidbody2D>();
     }
 
-    private void Start() {
-        AddStartingForce();
+    private void Start() 
+    {
+        ResetPosition();
     }
+
+    public void ResetPosition()
+        {
+            _rigidbody.position = Vector3.zero;
+            _rigidbody.velocity = Vector3.zero;
+
+            AddStartingForce();
+            
+        }
 
     private void AddStartingForce() {
         float x = Random.value < 0.5f ? -1.0f : 1.0f;
@@ -21,5 +32,13 @@ public class Ball : MonoBehaviour
         Vector2 direction = new Vector2(x, y);
         _rigidbody.AddForce(direction * this.speed);
     }
+
+    public void AddForce(Vector2 force)
+    {
+        _rigidbody.AddForce(force);
+    }
+
+    
+
 
 }
